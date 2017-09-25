94953175949517742e18b23b318eb4e2c370edff
孟倩 <mengqian@mengqiandeMacBook-Pro.local>
2017-09-25 21:59:07 +0800

    add assignment1

---
+++ 5002assignment1.py
@@ -0,0 +1,18 @@
+import numpy as np
+# Adding a dataset as in Table 1.
+Attributes1 = [-1, -2, -3, 1, 2, 3, 1]
+Attributes2 = [-1, -1, -2, 1, 1, 2, 2]
+Attributes3 = [1, 4, -2, 1, 2, 1, 4]
+# packed array
+data_set = np.vstack([Attributes1, Attributes2, Attributes3])
+# computing covariance matrix
+covariance_matrix = np.cov(data_set)
+print(covariance_matrix)
+# computing eigenvalues and eigenvectors
+from numpy import linalg as LA
+eigenvalues, eigenvectors = LA.eig(covariance_matrix)
+print(eigenvectors, eigenvalues)
+# ??
+eigenvalues.sort()
+proportion = sum(eigenvalues[-2:]) / sum(eigenvalues)
+print (proportion)
