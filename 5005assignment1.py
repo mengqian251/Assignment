1331a138b4e1a75666168d2ac9d7494cf4329121
孟倩 <mengqian@mengqiandeMacBook-Pro.local>
2017-09-28 20:34:25 +0800

    fix code

--- 5005assignment1.py
+++ 5005assignment1.py
@@ -1,12 +1,18 @@
 import re
 import urllib2
 from scrapy import Selector
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-target = "https://en.wikipedia.org/wiki/Yui_Aragaki"
+target = "https://en.wikipedia.org/wiki/The_Big_Bang_Theory"
 res = urllib2.urlopen(target)
 html = res.read()
 selector = Selector(text=html, type="html")
 text = selector.xpath('.//div[@id="mw-content-text"]').xpath('string(.)').extract_first()
 t = text.replace('\r', ' ').replace('\n', ' ').replace('\t', ' ').replace(',', ' ').strip()
 t = re.sub('\s+', ' ', t)
-print t
+# print t
+f = open("a.txt", "w")
+f.write(t)
+f.close()
